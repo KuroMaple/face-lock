7a02c426103aeb00b8c4a75732f842bc6fccf25a
Hassan <hashmihassan47@gmail.com>
2025-08-21 21:58:16 -0400

    began jwt auth

---
+++ face-lock-next/app/auth/page.tsx
@@ -0,0 +1,12 @@
+const auth = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  )
+}
+export default auth
